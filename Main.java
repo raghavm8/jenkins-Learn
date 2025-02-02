d28ce958a616daa3c74163798dd4e197dda14d9c
raghavm8 <raghav.mittal1109@gmail.com>
2025-02-02 15:07:47 +0530

    added a commit for jekins build

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is main class.....");
+        System.out.println("Sample for learning jenkins.....");
+    }
+}
